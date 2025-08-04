b43ba085266d8e2ea8b69f989b74dae7933a04ce
neerajhariyale <hariyaleneeraj31@gmail.com>
2025-08-05 02:28:34 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 // import { ShimmerButtonDemo } from './component/ShimmerButtonDemo'
 import Navbar from "./component/Navbar";

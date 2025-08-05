cbf5791251e7a98aee3d1af4f513a0e80a7fe56c
neerajhariyale <hariyaleneeraj31@gmail.com>
2025-08-05 12:12:42 +0530

    blur comiit

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -16,7 +16,7 @@
 
 
 
-
+import { BlurFade } from "@/components/magicui/blur-fade";
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react"; // Icon package like lucide-react or heroicons
@@ -42,19 +42,25 @@ const Navbar = () => {
   return (
     <div className="w-full  ">
       <div className="flex justify-between border-none items-center px-4 py-3 md:px-10 max-w-screen-xl mx-auto">
+        
         {/* Logo */}
         <div>
+          <BlurFade delay={0.25} inView>
           <img
             src={logo1}
             alt="Logo"
             className="h-18 w-18 cursor-pointer"
             onClick={() => navigate("/")}
           />
+          
+       </BlurFade>
         </div>
-
+        
         {/* Desktop Menu */}
         <div className="hidden md:flex gap-8 items-center">
+          
           {menuItems.map((item) => (
+            <BlurFade delay={0.25} inView>
             <p
               key={item.path}
               className={`cursor-pointer transition duration-100 ${
@@ -66,35 +72,51 @@ const Navbar = () => {
             >
               {item.name}
             </p>
+            </BlurFade>
           ))}
+          
         </div>
 
         {/* Donate Button */}
         <div className="hidden md:flex">
+ <BlurFade delay={0.25} inView>
           <button
             className="bg-white border-1 border-black rounded-full h-10 w-32 text-black hover:bg-pink-500 hover:text-white hover:border-1 hover:border-pink-500 transition hover:cursor-pointer"
             onClick={() => navigate("/donate")}
           >
             Donate
           </button>
+          </BlurFade>
         </div>
 
 
-
+  
         {/* Mobile View Tag Line AND Name */}
+
         <div className="md:hidden flex flex-col items-center p-1">
+          <BlurFade delay={0.25} inView>
           <h1 className="text-sm font-semibold text-black">The Helping Human Foundation</h1>
           <p className="text-[10px] text-gray-500 italic">"Give a hand, change a life"</p>
+          </BlurFade>
         </div>
 
         {/* Mobile Menu Toggle Button */}
+        
         <div className="md:hidden">
           <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
-            {isMobileMenuOpen ? <X size={26} /> : <Menu size={26} />}
+            
+            {isMobileMenuOpen ?  <X size={26} /> : <Menu size={26} />}
+            
           </button>
+         
         </div>
-      </div>
 
+        
+        
+      </div>
+        
+
+      
       {/* Mobile Dropdown Menu */}
       {isMobileMenuOpen && (
         <div className="md:hidden flex flex-col items-center gap-4 pb-4 bg-white shadow-md">

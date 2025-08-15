5e9ccc5a62bde32e03e7cba3c678f0d803e3d949
neerajhariyale <hariyaleneeraj31@gmail.com>
2025-08-15 20:02:11 +0530

    frontpage change

--- src/component/Aboutus.tsx
+++ src/component/Aboutus.tsx
@@ -1,19 +1,19 @@
 const Aboutus = () => {
   return (
     <div className="border border-gray-300 rounded-lg p-6 w-11/12 mx-auto bg-white shadow-md flex justify-center items-center flex-col">
-      <h2 className="text-2xl font-semibold  text-center mb-4">WITH YOU TODAY AND TOMORROW</h2>
+      <h2 className="text-xl font-semibold  text-center mb-4">WITH YOU TODAY AND TOMORROW</h2>
       <p className="mb-3 text-md">
-        Ravi Prakash Dahiya’s initiative to assist the homeless brings about significant
+        <span className="font-semibold">Ravi Prakash Dahiya’s</span> initiative to assist the homeless brings about significant
         transformations in their lives. The Helping Human Foundation shelters, they receive
         food, clothing, medical care, and a safe, hygienic environment. They find solace in
         the company of others who have faced similar challenges, creating a sense of belonging
         and connection.
       </p>
-      <p className="mb-2">
+      <p className="mb-2 font-semibold italic">
         Festivals and religious activities are celebrated together, fostering a supportive
         community.
       </p>
-      <p >
+      <p className="font-semibold italic">
         Volunteers offer companionship, a listening ear, and support to improve mental
         well-being.
       </p>
